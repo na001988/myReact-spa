96c20ff82425ce65783a68f2e890834b0970af0c
Roberto Zumba <rozumba@192-168-1-6.tpgi.com.au>
2020-04-15 14:38:04 +1000

    new app ready

---
+++ precache-manifest.b9c94077046e32ec1103594c17884978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d06f6c2aa812c2521e63d6502e4726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "903f865b05c194b54677",
+    "url": "/static/css/main.6e513df6.chunk.css"
+  },
+  {
+    "revision": "17cac3380b5a980a5efc",
+    "url": "/static/js/2.60c30cef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60c30cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "903f865b05c194b54677",
+    "url": "/static/js/main.8e98c413.chunk.js"
+  },
+  {
+    "revision": "7607309ca91d4c74d220",
+    "url": "/static/js/runtime-main.851496a5.js"
+  }
+]);
